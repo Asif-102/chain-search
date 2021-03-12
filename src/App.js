6231acb23d949d1ddf7c69f2631587eac1bab3bc
Asif2535 <aiqbal517@gmail.com>
2021-03-12 18:09:23 +0600

    implement search functionality in a react application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MealDetail from './components/MealDetail/MealDetail';
+import MealFinder from './components/MealFinder/MealFinder';
 
 function App() {
   return (
     <div className="App">
       <MealDetail></MealDetail>
+      <MealFinder></MealFinder>
     </div>
   );
 }
